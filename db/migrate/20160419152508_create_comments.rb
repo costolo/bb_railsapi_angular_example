4e28b9121d295d8024ae2c8c551cecc3ab617c8c
Ben <ben.costolo@gmail.com>
2016-04-19 11:50:40 -0400

    add comment model and db validations

--- db/migrate/20160419152508_create_comments.rb
+++ db/migrate/20160419152508_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.string :body
+      t.string :body, null: false
       t.references :post, index: true, foreign_key: true
       t.integer :vote, default: 0
 
