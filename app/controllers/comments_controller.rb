bc94bfd18cbb55aff3d4e4a1a5b07a852f3d0908
Ben <ben.costolo@gmail.com>
2016-04-19 14:05:18 -0400

    add create, upvote, and downvote methods to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def create
+    post = Post.find(params[:post_id])
+    comment = Post.comments.create(comment_params)
+    respond_with post, comment
+  end
+
+  def upvote
+    post = Post.find(params[:post_id])
+    comment = post.comments.find(params[:id])
+    comment.increment!(:votes)
+    respond_with post, comment
+  end
+
+  def downvote
+    post = Post.find(params[:post_id])
+    comment = post.comments.find(params[:id])
+    comment.decrement!(:votes)
+    respond_with post, comment
+  end
 end
