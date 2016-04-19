e5ddb2d24dad972a871297a94e50608786e628e9
Ben <ben.costolo@gmail.com>
2016-04-19 14:06:48 -0400

    add index, create, and show methods for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+  def index
+    respond_with Post.all
+  end
+
+  def create
+    respond_with Post.create(post_params)
+  end
+
+  def show
+    respond_with Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
