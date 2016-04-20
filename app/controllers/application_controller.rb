4708971fb27823e3e96aaff4dd07b60ba70203ff
Ben <ben.costolo@gmail.com>
2016-04-20 16:27:00 -0400

    add username to devise user create strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
   respond_to :json
+
   def angular
     render 'layouts/application'
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 end
