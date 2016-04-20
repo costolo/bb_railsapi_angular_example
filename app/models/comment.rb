e87c31a9c663a91c2756e8b4b943997db7075663
Ben <ben.costolo@gmail.com>
2016-04-20 16:41:11 -0400

    update post and comment as_json overrides to include the associated user object

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :post, :user
   validates :body, presence: true
+
+  def as_json(options = {})
+    super(options.merge(include: :user))
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   validates :title, :body, presence: true
 
-  def as_json(options={})
-    super(options.merge(include: :comments))
+  def as_json(options = {})
+    super(options.merge(include: [:user, comments: {include: :user}]))
   end
 end
