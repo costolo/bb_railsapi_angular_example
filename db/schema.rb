3ec9ac89c470846d37b434a6b1f50b3718bf36bf
Ben <ben.costolo@gmail.com>
2016-04-20 16:33:02 -0400

    add user references to post and comment tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420130023) do
+ActiveRecord::Schema.define(version: 20160420203100) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "body",                   null: false
@@ -19,16 +19,21 @@ ActiveRecord::Schema.define(version: 20160420130023) do
     t.integer  "votes",      default: 0
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",      null: false
     t.string   "body",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
