d04d136afc4b3e30e61cae7a258451e8aa86b8e8
Ben <ben.costolo@gmail.com>
2016-04-26 13:33:09 -0400

    set xsrf-token as a cookie after filter, override verified_request? to verify clientside xsrf token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
+  after_filter :set_csrf_cookie_for_ng
   respond_to :json
 
   def angular
     render 'layouts/application'
   end
 
-  private
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 
+  protected
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
+
+  private
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
   end
